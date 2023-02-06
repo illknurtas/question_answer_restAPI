4c2a99e26605849a2a8f303992d5ad5354b69aab
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-06 23:20:02 +0300

    config.env update

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({
+    path:"./config/env/config.env",
+});
 
 const app = express(); //creeating app
 
-const PORT =3000||process.env.PORT; 
+const PORT =process.env.PORT; 
 // process.env.PORT => is the port that assign default in different machines
 
 app.get("/", (req, res) => {
@@ -10,5 +15,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, ()=>{
-    console.log(`App started on PORT ${PORT}`);
+    console.log(`App started on PORT ${PORT}: ${process.env.NODE_ENV}`);
 });
