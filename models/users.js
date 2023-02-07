bc8501acf7acce71c89d8a1e72c905ea738fe439
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-08 00:17:55 +0300

    user model update

---
+++ models/users.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, "Please provide a name"]
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: [true,"Please try different email address"],
+        match: [
+            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+            "Please provide a valid email address"
+        ]
+    },
+    role:{
+        type: String,
+        default:"user",
+        enum: ["user", "admin"] 
+    },
+    password:{
+        type: String,
+        minlength:[6,"Please provide a password with at least 6 characters"],
+        required:[true,"Please provide a valid password"],
+        select: false
+    },
+    createDate:{
+        type: Date,
+        default: Date.now
+    },
+    title:{
+        type: String,
+    },
+    about:{
+        type: String,
+    },
+    place:{
+        type: String,
+    },
+    website:{
+        type: String,
+    },
+    profile_img:{
+        type: String,
+        default:"default.jpg"
+    },
+    blocked:{
+        type: Boolean,
+        default:false
+    }
+})
