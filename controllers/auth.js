aee71bc20eee4efd80135fe560beb4d021d39b02
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-08 19:25:54 +0300

    add user update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,20 @@
-const register = (req, res, next) => {
+const User = require ('../models/user');
+const register = async (req, res, next) => {
+    // POST DATA 
+    const name ="İlayda günevi Joubert";
+    const email ="deneme3@gmail.com";
+    const password ="12345";
+
+    // async await
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
     res.status(200).json({
         success : true,
+        data: user
     })
 }
 
