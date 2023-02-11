be226a8adde5a71db8f2c4de5fd088f9d1d25b1b
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-12 00:29:40 +0300

    all users update

--- controllers/user.js
+++ controllers/user.js
@@ -7,13 +7,22 @@ const getSingleUser = asyncErrorWrapper(async(req, res, next)=>{
     const {id} = req.params;
 
     const user = await User.findById(id);
-    
+
     return res.status(200).json({
         success: true,
         data: user
     });
 });
 
+const getAllUsers = asyncErrorWrapper(async(req, res, next)=>{
+    const users = await User.find();
+    return res.status(200).json({
+        success: true,
+        data: users
+    });
+});
+
 module.exports = {
-    getSingleUser
+    getSingleUser,
+    getAllUsers
 }
