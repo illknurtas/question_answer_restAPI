56d481ffeac72804e541dfc975b022fd7cf2939f
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-13 19:01:47 +0300

    slug added

--- models/que.js
+++ models/que.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 
 const Schema = mongoose.Schema;
 const questionSchema = new Schema({
@@ -13,9 +15,7 @@ const questionSchema = new Schema({
         required: [true,"Please enter a content"],
         minlenth:[20,"Please provide a content at least 10 characters length" ],
     },
-    slug:{
-        type: String,
-    },
+    slug:String,
     createdAt:{
         type: Date,
         default: Date.now
@@ -26,5 +26,25 @@ const questionSchema = new Schema({
         required: true
     }
 });
+
+questionSchema.pre("save", function(next){
+    if(!this.isModified("title")){
+        next();
+    }
+    this.slug = this.makeSlug();
+    next();
+});
+
+questionSchema.methods.makeSlug = function(){
+    return slugify(this.title,{
+        replacement: '-',  // replace spaces with replacement character, defaults to `-`
+        remove: /[*+~.()'"!:@]/g, // remove characters that match regex, defaults to `undefined`
+        lower: true,      // convert to lower case, defaults to `false`
+        // strict: false,     // strip special characters except replacement, defaults to `false`
+        // locale: 'vi',       // language code of the locale to use
+        // trim: true         // trim leading and trailing replacement chars, defaults to `true`
+    });
+    
+};
 
 module.exports = mongoose.model ("Questions",questionSchema);
