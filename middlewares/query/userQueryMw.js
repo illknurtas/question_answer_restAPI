a00ba5088ab5e82e91661468cc57efd418dd299b
illknurtas <77456662+illknurtas@users.noreply.github.com>
2023-02-17 16:40:00 +0300

    user query mw centralized

---
+++ middlewares/query/userQueryMw.js
@@ -0,0 +1,30 @@
+const { request } = require("express");
+const asyncErrorWrapper = require ("express-async-handler");
+const { 
+    searchHelper, 
+    paginationHelper } = require("./queryMwHelpers");
+ 
+
+const userQueryMw = function(model, options) {
+    return asyncErrorWrapper(async function (req, res, next) {
+        let query = model.find();
+
+        query = searchHelper("name", query, req);
+
+        const paginationResult = await paginationHelper(model, query, req);
+
+        query = paginationResult.query;
+        pagination = paginationResult.pagination;
+
+        const queryResults = await query;
+        res.queryResults = {
+            success: true,
+            count: queryResults.length,
+            pagination: pagination,
+            data: queryResults
+        }
+        next();
+    })
+};
+
+module.exports = userQueryMw;
--- routers/user.js
+++ routers/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {getSingleUser, getAllUsers} = require("../controllers/user");
 const {checkUserExist}= require ("../middlewares/database/databaseErrorHelpers");
+const userQueryMw = require("../middlewares/query/userQueryMw");
+const User =  require ("../models/user");
 
 const router = express.Router();
 
-router.get("/", getAllUsers);
+router.get("/", userQueryMw(User) , getAllUsers);
 router.get("/:id", checkUserExist, getSingleUser);
 
 module.exports = router;
